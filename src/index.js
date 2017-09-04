46bb0078e0a243d8040d44f657f39f3dde8a7f03
Zero Zhang <southbean2013@gmail.com>
2017-09-04 15:09:33 +0800

    [Add] hit detection

--- src/index.js
+++ src/index.js
@@ -64,6 +64,8 @@ class Bullet {
   constructor () {
     this.dx = Plane.dx + 30
     this.dy = Plane.dy - 10
+    this.w = 1
+    this.h = 10
     this.vy = 10
     this.ani = null
     this.move()
@@ -73,13 +75,13 @@ class Bullet {
     Game.ctx.clearRect(this.dx, this.dy, 1, this.vy)
     this.dy -= this.vy
     if (this.dy < 20) {
-      this.ani = null
+      window.cancelAnimationFrame(this.ani)
     } else {
       if (this.dy < 30) {
         Game.ctx.fillRect(this.dx, 30, 1, 10 - (30 - this.dy))
       }
       Game.ctx.fillRect(this.dx, this.dy, 1, 10)
-      this.ani = window.requestAnimationFrame(() => this.move())
+      // this.ani = window.requestAnimationFrame(() => this.move())
     }
   }
 }
@@ -88,6 +90,8 @@ class Monster {
   constructor (_dx = 30, _dy = 30) {
     this.dx = _dx
     this.dy = _dy
+    this.w = 50
+    this.h = 50
     this.vx = 2
     this.ani = null
     this.img = null
@@ -127,14 +131,22 @@ class MonsterGroup {
     this.dx = 30
     this.dy = 30
     this.vx = 2
+    this.flagArr = []
+    for (let i = 0; i < 50; i++) {
+      this.flagArr[i] = new Array(this.num * 60 - 10)
+      this.flagArr[i].fill(0)
+    }
   }
   view () {
     for (let i = 0; i < this.num; i++) {
       let monster = new Monster((50 + this.gap) * i + this.dx, this.dy)
+      for (let i = 0; i < 50; i++) {
+        this.flagArr[i].fill(1, this.dx, this.dx + 51)
+      }
       this.list.push(monster)
-      setTimeout(() => {
-        monster.die = true
-      }, 2000 * (i + 1))
+      // setTimeout(() => {
+      //   monster.die = true
+      // }, 2000 * (i + 1))
       monster.view()
     }
     this.dxr = (50 + this.gap) * this.num
@@ -171,7 +183,7 @@ class MonsterGroup {
         _ele.move()
       }
     })
-    this.ani = window.requestAnimationFrame(() => this.move())
+    // this.ani = window.requestAnimationFrame(() => this.move())
   }
 }
 
@@ -187,6 +199,7 @@ let Plane = {
     }
     self.img.src = planeImg
     self.ani = null
+    self.bulletList = []
     self.listen()
   },
   listen: function () {
@@ -216,6 +229,7 @@ let Plane = {
         break
       case 32:
         let bull = new Bullet()
+        self.bulletList.push(bull)
         break
       default:
         break
@@ -234,13 +248,53 @@ let Plane = {
 let Game = {
   init: function () {
     let self = this
+    self.plane = null
+    self.enemies = null
     self.ctx = $('#J_game')[0].getContext('2d')
     self.view()
   },
   view: function () {
-    Plane.view()
-    let enemies = new MonsterGroup()
-    enemies.view()
+    let self = this
+    self.plane = Plane
+    self.plane.view()
+    self.enemies = new MonsterGroup()
+    self.enemies.view()
+    self.move()
+  },
+  hitDetection: function () {
+    let self = this
+    let bullets = self.plane.bulletList
+    let monsters = self.enemies.list
+    bullets.forEach((_ele) => {
+      let _eleDx = _ele.dx,
+          _eleDy = _ele.dy,
+          _eleW = _ele.w,
+          _eleH = _ele.h
+
+      monsters.forEach((_item) => {
+        if (_item.die || _item.dead) {
+          return
+        }
+        let _itemDx = _item.dx,
+            _itemDy = _item.dy,
+            _itemW = _item.w,
+            _itemH = _item.h
+        if (_eleDy > _itemDy + _itemH || _eleDy + _eleH < _itemDy || _eleDx < _itemDx || _eleDx + _eleW > _itemDx + _itemW) {
+        } else {
+          _item.die = true
+        }
+      })
+    })
+  },
+  move: function () {
+    let self = this
+    let bullets = self.plane.bulletList
+    bullets.forEach((_ele) => {
+      _ele.move()
+    })
+    self.enemies.move()
+    self.hitDetection()
+    self.ani = window.requestAnimationFrame(() => self.move())
   }
 }
 
